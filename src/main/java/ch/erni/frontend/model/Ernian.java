537a10a288734ca08eb39b5be6c881faf9d1151b
DESKTOP-OE020K2\matth <burkertmatthias1@googlemail.com>
2018-06-19 19:48:32 +0200

    Correct trailing spaces style problem

--- src/main/java/ch/erni/frontend/model/Ernian.java
+++ src/main/java/ch/erni/frontend/model/Ernian.java
@@ -31,55 +31,42 @@ public class Ernian {
     public UUID getId() {
         return id;
     }
-    
     public void setId(UUID id) {
         this.id = id;
     }
-    
     public String getFirstName() {
         return firstName;
     }
-    
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
     public String getLastName() {
         return lastName;
     }
-    
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-    
     public Locale getCountry() {
         return country;
     }
-    
     public void setCountry(Locale country) {
         this.country = country;
     }
-    
     public String getTown() {
         return town;
     }
-    
     public void setTown(String town) {
         this.town = town;
     }
-    
     public String getZip() {
         return zip;
     }
-    
     public void setZip(String zip) {
         this.zip = zip;
     }
-    
     public String getStreet() {
         return street;
     }
-    
     public void setStreet(String street) {
         this.street = street;
     }
